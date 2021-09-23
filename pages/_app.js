64ec57fe19bd2000847e9557d7e32e1d9e63e759
Philippe Bernard <philippe.bernard@gmail.com>
2021-09-23 10:51:14 +0200

    Open Graph support

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import { FacebookOpenGraph } from '@resoc/core'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>{pageProps.title}</title>
+        <meta name="description" content={pageProps.description} />
+
+        <meta property="og:title" content={pageProps.title} />
+        <meta property="og:description" content={pageProps.description} />
+        <meta property="og:image" content={`/social-images/${pageProps.imgSlug}`} />
+        <meta property="og:image:width" content={FacebookOpenGraph.width} />
+        <meta property="og:image:height" content={FacebookOpenGraph.height} />
+
+        <link rel="icon" href="/resoc.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
