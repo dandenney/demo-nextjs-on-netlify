21c4a3f1d90c87f613432e5ccb63db5f19c30a6c
Philippe Bernard <philippe.bernard@gmail.com>
2021-09-23 09:20:27 +0200

    Use @resoc/img-data instead of @resoc/create-img

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { storeImageData } from '@resoc/create-img'
+import { storeImageData } from '@resoc/img-data'
 
 export default function Home() {
   return (
