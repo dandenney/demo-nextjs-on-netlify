801676818f0f297f7620335c4c3f6014163627d8
Philippe Bernard <philippe.bernard@gmail.com>
2021-09-24 14:34:46 +0200

    Content

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,33 @@ import { storeImageData } from '@resoc/img-data'
 export default function Home() {
   return (
     <div className={styles.container}>
-      No need for content, really
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Automated Social Images on Netlify
+          <br/>
+          With Next.js
+        </h1>
+
+        <p className={styles.description}>
+          Using the <a href="https://www.npmjs.com/package/itdk">
+            Resoc image template development kit
+          </a> and <a href="https://www.npmjs.com/package/@resoc/netlify-plugin-social-image">
+            Resoc social image Netlify build plugin
+          </a>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://developers.facebook.com/tools/debug" className={styles.card}>
+            <h2>Try &rarr;</h2>
+            <p>Submit this page to the Facebook debugger, or share it on a social network.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Follow the tutorial and build this app yourself.</p>
+          </a>
+        </div>
+      </main>
     </div>
   )
 }
